a02fb6a79fae669756b4bab065ae7bb52d3dbcb5
ppengotsu <ppengotsu@ppen.info>
2020-07-01 01:37:20 +0900

    クリップボードにコピーまで実装

--- Assets/MySceneController.cs
+++ Assets/MySceneController.cs
@@ -2,27 +2,55 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Net.NetworkInformation;
+using UnityEngine.UI;
 
 
 namespace PPEN
 {
     public class MySceneController : MonoBehaviour
     {
+
+        [SerializeField]
+        private Text wifiAlertText;
+
+        [SerializeField]
+        private InputField macaddressInputField;
+
+
+
         // Start is called before the first frame update
         void Start()
         {
-            Debug.Log(GetPhysicalAddressListStg());
+            this.macaddressInputField.text = GetWIFIPhysicalAddressListStg();
         }
 
         // Update is called once per frame
         void Update()
         {
-
+            if(IsUpWIFI() == false)
+            {
+                //WIFIが有効でない場合
+                this.wifiAlertText.enabled = true;
+            }
+            else
+            {
+                this.wifiAlertText.enabled = false;
+            }
+        
         }
 
+        public void TouchedUpdateMacAddress()
+        {
+            this.macaddressInputField.text = GetWIFIPhysicalAddressListStg();
+        }
+    
 
 
 
+        public void TouchedCopyClipboard()
+        {
+            GUIUtility.systemCopyBuffer = GetWIFIPhysicalAddressListStg();
+        }
 
 
 
@@ -31,7 +59,7 @@ namespace PPEN
 
 
         //参考：https://araramistudio.jimdo.com/2019/05/20/c-%E3%81%A7mac%E3%82%A2%E3%83%89%E3%83%AC%E3%82%B9%E3%82%92%E5%8F%96%E5%BE%97%E3%81%99%E3%82%8B/
-        public string GetPhysicalAddressListStg()
+        public string GetWIFIPhysicalAddressListStg()
         {
             var reText = "";
             var interfaces = NetworkInterface.GetAllNetworkInterfaces();
@@ -45,18 +73,19 @@ namespace PPEN
                     if ((NetworkInterfaceType.Ethernet == adapter.NetworkInterfaceType))
                     {
                         //有線LAN
-                        reText += "状態UP：Ethernet：Mac Address = " + adapter.GetPhysicalAddress() + "\n";
+                        //reText += "状態UP：Ethernet：Mac Address = " + adapter.GetPhysicalAddress() + "\n";
 
                     }
 
                     if ((NetworkInterfaceType.Wireless80211 == adapter.NetworkInterfaceType))
                     {
                         //WIFI
-                        reText += "状態UP：WIFI：Mac Address = " + adapter.GetPhysicalAddress() + "\n";
+                        // reText += "状態UP：WIFI：Mac Address = " + adapter.GetPhysicalAddress() + "\n";
+                        return "" + adapter.GetPhysicalAddress();
 
                     }
                 }
-                Debug.Log(adapter.Name);
+              //  Debug.Log(adapter.Name);
 
             }
             return reText;
